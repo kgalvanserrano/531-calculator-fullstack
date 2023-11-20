ab1f6c30b9f4f49aaf0a0ce5cd21fb313a2ea478
Kevin <kevin.galvanserrano@gmail.com>
2023-11-20 02:10:48 -0800

    first commit

--- backend/server.js
+++ backend/server.js
@@ -17,3 +17,16 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log('Server is running on port ${PORT}');
 });
+
+// New endpoint to handle calculator logic
+app.post('/api/calculate', (req, res) => {
+    const { lifts } = req.body;
+  
+    // Perform your 5/3/1 Calculator logic here
+    // For now, just return the received data
+    res.json({ message: 'Calculation completed', lifts });
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
